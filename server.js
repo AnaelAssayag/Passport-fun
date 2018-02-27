037588b0e2e72005fd8fe41cdf6197ebdd8611a1
Anael Dahan <anael.assayag@gmail.com>
2018-02-27 14:11:51 +0200

    add session

--- server.js
+++ server.js
@@ -1,42 +1,59 @@
-// Get dependencies
 const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+var LocalStrategy = require('passport-local').Strategy;
+const expressSession=require('express-session');
+
 const app = express();
 
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 //Authentication middleware
+app.use(expressSession({ secret: 'thisIsASecret', resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+    console.log(user);
+  done(null, user);
+});
+
+passport.use(new LocalStrategy(
+//    { passReqToCallback : true},
+  function(username, password, done) {
+    if ((username === "john") && (password === "password")) {
+      return done(null, { username: username, id: 1 });
+    } else {
+      return done(null, false, "Failed to login.");
+    }
+  }
+));
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/login.html'));
-});
+
 app.post('/login', passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/login?err',
-  session: false
 }));
-passport.use(new LocalStrategy(function(username, password, done) {
-  if ((username === "john") && (password === "password")) {
-    return done(null, { username: username, id: 1 });
-  } else {
-    return done(null, false);
-  }
-}));
+
+// Catch all other routes and return the index file
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/login.html'));
+});
+
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
+ // Catch all other routes and return the index file
+app.use((err, req, res, next) => {
+    res.status(500).send(err);
+  });
 
-
- 
 const port = process.env.PORT || '3000';
 app.set('port', port);
  
