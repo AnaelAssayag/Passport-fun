a4398905e295202405f4ea9e4cf8a47175c9dd69
Anael Dahan <anael.assayag@gmail.com>
2018-02-27 14:01:40 +0200

    add route & login possibility for 'john'

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const app = express();
 
 // Parsers for POST data
@@ -14,10 +15,21 @@ app.use(passport.initialize());
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
-
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/login.html'));
-}); 
+});
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login?err',
+  session: false
+}));
+passport.use(new LocalStrategy(function(username, password, done) {
+  if ((username === "john") && (password === "password")) {
+    return done(null, { username: username, id: 1 });
+  } else {
+    return done(null, false);
+  }
+}));
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
